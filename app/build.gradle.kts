e2799c77a68e1cd30a568360b18dea08b113bd2c
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-09-28 14:58:42 -0400

    Exercise: Implement Data Binding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    dataBinding {
+        //noinspection DataBindingWithoutKapt
+        enable = true
+        }
 }
 
 dependencies {
@@ -60,6 +64,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.databinding.runtime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

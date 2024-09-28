b5e0699826f14793de7da83d85a3e76e801beba6
Gilbert <gilberto.ramirez.suazo@gmail.com>
2024-09-28 16:42:52 -0400

    Exercise: Add Aligned Boxes with Click Handlers

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,47 +1,30 @@
 package com.example.colormyviews
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.colormyviews.ui.theme.ColorMyViewsTheme
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ColorMyViewsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.box_one_text).setOnClickListener { makeColored(it) }
+        findViewById<View>(R.id.box_two_text).setOnClickListener { makeColored(it) }
+        findViewById<View>(R.id.box_three_text).setOnClickListener { makeColored(it) }
+        findViewById<View>(R.id.box_four_text).setOnClickListener { makeColored(it) }
+        findViewById<View>(R.id.box_five_text).setOnClickListener { makeColored(it) }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ColorMyViewsTheme {
-        Greeting("Android")
+    fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
